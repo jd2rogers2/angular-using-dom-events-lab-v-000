d96db4660a2b8e875c743efd4e6b9f5fbfacd74f
jd2rogers2 <jd2rogers2@gmail.com>
2016-11-03 14:58:21 -0400

    donzo

--- js/app/directives/Counter.js
+++ js/app/directives/Counter.js
@@ -4,11 +4,23 @@ function Counter() {
 			'<div>',
 				'<h3>Counter</h3>',
 				'<div>Click anywhere to increment the counter!</div>',
-				'<div>Current count: {{ count }}</div>',
+				'<div>Current count: {{ ctrl.count }}</div>',
 			'</div>'
 		].join(''),
-		controller: function ($scope) {
-			$scope.count = 0;
+		controller: function () {
+			this.count = 0;
+		},
+		require: 'counter',
+		controllerAs: 'ctrl',
+		link: function(scope, element, attrs, ctrl){
+			document.addEventListener("click", function(){
+				ctrl.count++;
+				scope.$apply();
+			});
+
+			scope.$on("destroy", function(){
+				document.off();
+			});
 		}
 	}
 }
